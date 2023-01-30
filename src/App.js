2be786f92e8ea320194d99c60ff2570c4a74be74
Akshay Raut <rautakshay1422@gmail.com>
2023-01-30 16:57:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HabitContainer from "./components/HabitContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Habit Tracker App</h1>
+      <HabitContainer />
+    </div>
+  );
+}
+
+export default App;
